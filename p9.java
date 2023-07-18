001ad66cdeca1caa8a16b3e8befccbdf228c1a95
pratyush7728 <94605742+pratyush7728@users.noreply.github.com>
2023-07-18 08:22:36 +0530

    Create p9.java

---
+++ p9.java
@@ -0,0 +1,27 @@
+// declare the interfaces
+interface subtraction
+{
+    void sub(int a,int b);
+}
+interface addition
+{
+    void add(int a,int b);
+}
+class operate implements subtraction,addition{                // implements the interfaces subtraction, addition
+    public void sub(int a,int b){
+        int diff=a-b;
+        System.out.println("Difference of a & b is "+ diff);
+    }                                               // sub ends
+    public void add(int a,int b){
+        int sum=a+b;
+        System.out.println("Sum of a & b is "+ sum);
+    }                                               // add ends
+}
+
+public class p9 {
+    public static void main(String args[]){          // main function
+        operate obj = new operate();           // creation of object
+        obj.sub(20,10);
+        obj.add(20,10);
+    }                                   // main ends
+}                                       // class ends
